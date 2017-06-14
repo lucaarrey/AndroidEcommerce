2c7baabcb343e70807542a4a38f6b6c5a5add659
Luca Bandini <luca.bandini@outlook.it>
2017-06-14 16:19:37 +0200

    added snackbar with undo action

--- app/src/main/java/sophia/com/ecommerce2/ProductListActivity.java
+++ app/src/main/java/sophia/com/ecommerce2/ProductListActivity.java
@@ -1,9 +1,12 @@
 package sophia.com.ecommerce2;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -33,7 +36,7 @@ public class ProductListActivity extends AppCompatActivity implements OnAdapterI
 
 
         for (int i =0; i < 50; i++){
-            productlist.add(new Product(1, "http://clicksolutions.it/wp-content/uploads/2016/04/privati.jpg","Pc", 13.2, "blalvvlalvlalvallv" ));
+            productlist.add(new Product(1, "http://clicksolutions.it/wp-content/uploads/2016/04/privati.jpg","Pc", 13.99, "blalvvlalvlalvallv" ));
         }
 
         ProductAdapter productAdapter = new ProductAdapter(productlist, this);
@@ -45,5 +48,27 @@ public class ProductListActivity extends AppCompatActivity implements OnAdapterI
     @Override
     public void OnItemClick(int position) {
 
+    }
+
+    @Override
+    public void OnAddToCartClick(int position) {
+
+        Log.d("ProductListActivity","Stampato add to cart: " + position);
+
+        Snackbar mySnackbar = Snackbar.make(findViewById(R.id.product_recycle_view),
+                "added to cart", Snackbar.LENGTH_SHORT);
+        mySnackbar.setAction("annulla", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("ProductListActivity","Annullato");
+            }
+        });
+        mySnackbar.show();
+    }
+
+    @Override
+    public void OnShareClick(int position) {
+
+        Log.d("ProductListActivity","Stampato share" + position);
     }
 }
