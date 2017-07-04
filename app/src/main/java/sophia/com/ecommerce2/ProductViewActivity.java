b5125d7652744df1e42c4d76aa6b5579293b6bd3
Luca Bandini <luca.bandini@outlook.it>
2017-07-04 17:39:47 +0200

    change layout: color price and title

--- app/src/main/java/sophia/com/ecommerce2/ProductViewActivity.java
+++ app/src/main/java/sophia/com/ecommerce2/ProductViewActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import sophia.com.ecommerce2.Database.EcommerceOpenHelper;
 import sophia.com.ecommerce2.model.Product;
 
 public class ProductViewActivity extends AppCompatActivity {
@@ -18,10 +19,14 @@ public class ProductViewActivity extends AppCompatActivity {
     public TextView description;
     public TextView long_description;
 
+    private EcommerceOpenHelper ecommerceDB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_view);
+
+        ecommerceDB = new EcommerceOpenHelper(this);
 
         img_product = (ImageView)findViewById(R.id.img_product);
         name = (TextView) findViewById(R.id.name);
@@ -30,19 +35,26 @@ public class ProductViewActivity extends AppCompatActivity {
         long_description = (TextView) findViewById(R.id.long_description);
 
 
-        Product p = new Product(1,
-                "http://clicksolutions.it/wp-content/uploads/2016/04/privati.jpg",
-                "PC",
-                43.99,
-                "adfnadkònf",
-                3,
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec eros interdum felis faucibus hendrerit ac at risus. Nulla convallis id urna id bibendum. Curabitur maximus sem imperdiet enim tristique, in euismod quam aliquam. Proin sed dui vitae dolor suscipit lobortis vel et ipsum. Praesent at est sapien. Aliquam suscipit dolor convallis turpis molestie hendrerit. In condimentum rutrum dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis neque est, pharetra eget elementum eu, convallis et nibh. Integer gravida suscipit felis sit amet aliquam. Mauris tempor enim non neque aliquet volutpat. Pellentesque sed nunc vitae." );
+//        Product p = new Product(1,
+//                "http://clicksolutions.it/wp-content/uploads/2016/04/privati.jpg",
+//                "PC",
+//                43.99,
+//                "adfnadkònf",
+//                3,
+//                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec eros interdum felis faucibus hendrerit ac at risus. Nulla convallis id urna id bibendum. Curabitur maximus sem imperdiet enim tristique, in euismod quam aliquam. Proin sed dui vitae dolor suscipit lobortis vel et ipsum. Praesent at est sapien. Aliquam suscipit dolor convallis turpis molestie hendrerit. In condimentum rutrum dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis neque est, pharetra eget elementum eu, convallis et nibh. Integer gravida suscipit felis sit amet aliquam. Mauris tempor enim non neque aliquet volutpat. Pellentesque sed nunc vitae."
+//                 1);
+
+//
+        int productId = getIntent().getIntExtra("productId", -1);
+
+        Product p = ecommerceDB.getProduct(productId);
 
         Picasso.with(this).load(p.getImagePath()).into(img_product);
         name.setText(p.getName());
         stars.setTag(p.getStars());
         description.setTag(p.getDescription());
         long_description.setText(p.getLongDescription());
+
 
 
     }
