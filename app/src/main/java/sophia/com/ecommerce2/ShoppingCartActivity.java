597f91233cde9bcaae0e803270c846fed2d1d7fb
Luca Bandini <luca.bandini@outlook.it>
2017-07-13 17:17:27 +0200

    change layout: color price and title

--- app/src/main/java/sophia/com/ecommerce2/ShoppingCartActivity.java
+++ app/src/main/java/sophia/com/ecommerce2/ShoppingCartActivity.java
@@ -4,17 +4,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.TextView;
 
-public class ShoppingCartActivity extends AppCompatActivity {
+import java.text.NumberFormat;
+import java.util.Locale;
+
+import sophia.com.ecommerce2.Cart.ShoppingCart;
+import sophia.com.ecommerce2.adapter.OnAdapterItemClickListener;
+import sophia.com.ecommerce2.adapter.ShoppingCartAdapter;
+
+public class ShoppingCartActivity extends AppCompatActivity implements OnAdapterItemClickListener{
 
     private RecyclerView shoppingCartRecycleView;
-    private RecyclerView.LayoutManager shoppinCartLayoutManager;
+    private RecyclerView.LayoutManager shoppingCartLayoutManager;
 
+    private TextView total;
+    private NumberFormat format;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_cart);
+
+        format = NumberFormat.getCurrencyInstance(Locale.ITALY);
 
 
         shoppingCartRecycleView = (RecyclerView) findViewById(R.id.shopping_cart_recycleView);
@@ -22,6 +36,42 @@ public class ShoppingCartActivity extends AppCompatActivity {
         shoppingCartRecycleView.setLayoutManager(new LinearLayoutManager(this));
         shoppingCartRecycleView.setHasFixedSize(true);
 
+        ShoppingCartAdapter shoppingCartAdapter = new ShoppingCartAdapter(this);
+
+        shoppingCartRecycleView.setAdapter(shoppingCartAdapter);
+
+        total = (TextView) findViewById(R.id.total);
+
+        total.setText(format.format(ShoppingCart.getInstance().totalAmount()));
+    }
+
+
+    @Override
+    public void OnItemClick(int position) {
 
     }
+
+    @Override
+    public void OnAddToCartClick(int position) {
+
+    }
+
+    @Override
+    public void OnShareClick(int position) {
+
+    }
+
+    @Override
+    public void OnRemoveToCartClick() {
+
+        ShoppingCart.getInstance().totalAmount();
+
+        total.setText(format.format(ShoppingCart.getInstance().totalAmount()));
+
+
+    }
+
+
+
+
 }
